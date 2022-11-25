2185714ed1b139d04d8cdf3cc376e6ff7f0a3c87
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-11-25 18:10:12 +0600

    я закончил дз

--- src/components/Countrys/Countrys.tsx
+++ src/components/Countrys/Countrys.tsx
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { Country } from '../../types'
+import { Country, Selected } from '../../types'
+import NewCountry from '../NewCountry/NewCountry'
 import SelectedCountry from '../SelectedCountry/SelectedCountry'
 import './Countrys.css'
 
 const URL_COUNTRYS = 'https://restcountries.com/v2/all?fields=alpha3Code,name'
+const URL_COUNTRY = 'https://restcountries.com/v2/alpha/'
 
 const Countrys = () => {
     const [countrys , setCountrys] = useState<Country[]>([])
@@ -13,14 +15,28 @@ const Countrys = () => {
         setCountrys(responseCountry.data)
     }
     fechCountry()
+
+    const [select , setSelect] = useState<JSX.Element | null>(null)
+
+    const fechSelektedCountry = async(code: string) => {
+        const responseSelectCountry = await axios.get<Selected>(URL_COUNTRY + code)
+        setSelect(<NewCountry 
+            borders={responseSelectCountry.data.borders}
+            capital={responseSelectCountry.data.capital}
+            flag={responseSelectCountry.data.flag}
+            name={responseSelectCountry.data.name}
+            />)
+    }
   return (
-    <div>
+    <div className='Basic'>
       <div className='Countrys'>
-        {countrys.map(country => (
-        <SelectedCountry code={country.alpha3Code} name={country.name}/>
+        {countrys.map((country , index) => (
+        <SelectedCountry key={index} fechSelektedCountry={fechSelektedCountry}  code={country.alpha3Code} name={country.name}/>
       ))}
       </div>
-      <div></div>
+      <div className='NewCountry'>
+        {select}
+      </div>
     </div>
   )
 }
--- src/components/SelectedCountry/SelectedCountry.tsx
+++ src/components/SelectedCountry/SelectedCountry.tsx
@@ -4,33 +4,17 @@ import { Selected } from '../../types';
 interface Props {
     name: string;
     code: string;
+    fechSelektedCountry: (code: string) => void
 }
 
 const URL_COUNTRY = 'https://restcountries.com/v2/alpha/'
 
-const SelectedCountry:React.FC<Props> = ({code , name}) => {
+const SelectedCountry:React.FC<Props> = ({code , name , fechSelektedCountry}) => {
 
-    const [country , setCountry] = useState<Selected | null>(null)
-
-
-    const fechSelektedCountry = async() => {
-        const responseSelectCountry = await axios.get<Selected>(URL_COUNTRY + code)
-        setCountry(prev => {
-            return {
-                borders: responseSelectCountry.data.borders,
-                capital: responseSelectCountry.data.capital,
-                name: responseSelectCountry.data.name,
-                flag: responseSelectCountry.data.flag
-            }
-        })
-    }
-
-    useEffect(() => {
-        console.log(country)
-    } , [country])
+    
   return (
     <div>
-        <div className='SelectedCountry' onClick={fechSelektedCountry}>{name}</div>
+        <div className='SelectedCountry' onClick={() => fechSelektedCountry(code)}>{name}</div>
     </div>
   )
 }
