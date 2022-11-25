e6dc967afee51998382b09847dc64fc1bd52c446
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-11-25 13:39:41 +0600

    Я создал файл с типизацией запрашиваемых с сервера объектов

--- src/components/Countrys/Countrys.tsx
+++ src/components/Countrys/Countrys.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Country } from '../../types'
+const URL_COUNTRYS = 'https://restcountries.com/v2/all?fields=alpha3Code,name'
 
 const Countrys = () => {
+    const [countrys , setCountrys] = useState<Country[]>([])
+    const fechCountry = async () => {
+        const responseCountry = axios.get(URL_COUNTRYS)
+        console.log(responseCountry)
+    }
+    fechCountry()
   return (
     <div>
       
